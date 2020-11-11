6e5f14d63ca0b6e08e92e527f9d38fba578b1316
Alvaro <alvaroortiz.ballesteros@gmail.com>
2020-11-11 18:45:27 +0100

    probando intellij

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,37 @@
+import java.util.Objects;
+
 public class Persona {
-    
+    String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return Objects.equals(nombre, persona.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
 }
